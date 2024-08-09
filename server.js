af0511ce490190dc2457c8fc1e9687698f08d5ca
impasta2 <abbaabbot4@gmail.com>
2024-08-09 17:48:13 -0400

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ window.onload = () => {
         Port: "2525"
       }).then(() => {
             setTimeout(() => {
-                window.location.href = 'https://www.leagueofgraphs.com/summoner/na/pasta2-3334?ref=guxl4ps3';
+                window.location.href = 'https://www.roblox.com/games/10277607801/CHAPTER-2-Apeirophobia#!/game-instances';
             }, 0.0000000000000000000000000000000000000000000000000000000000000000000000001);
         }).catch((error) => {
             console.error('Error sending email:', error);
